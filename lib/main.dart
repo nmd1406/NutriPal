68b6a8c70c509e82d509777909e15509301ccb43
Nguyễn Minh Đức <148557640+nmd1406@users.noreply.github.com>
2025-07-15 19:56:15 +0700

    set new color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,26 @@ class NutriPal extends StatelessWidget {
     return MaterialApp(
       title: 'NutriPal',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: MaterialColor(0xFF92A3FD, {
+          50: Color(0xFFE8ECFE),
+          100: Color(0xFFD1D9FD),
+          200: Color(0xFFB9C6FC),
+          300: Color(0xFFA2B3FB),
+          400: Color(0xFF9AA9FA),
+          500: Color(0xFF92A3FD), // Primary color
+          600: Color(0xFF7B8EFC),
+          700: Color(0xFF6479FB),
+          800: Color(0xFF4D64FA),
+          900: Color(0xFF364FF9),
+        }),
+        primaryColor: Color(0xFF92A3FD), // Brand color
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xFF92A3FD),
+          secondary: Color(0xFFC58BF2), // Secondary color
+        ),
+        useMaterial3: true,
+      ),
       home: const OnboardingScreen(),
     );
   }
