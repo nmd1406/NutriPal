e8a81871c66622efe40a3f7d1c8d24eeb5ac71b1
Nguyễn Minh Đức <148557640+nmd1406@users.noreply.github.com>
2025-07-17 00:15:51 +0700

    refactor Profile model to include uid and update copyWith, toJson, and fromJson methods

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -1,18 +1,27 @@
 class Profile {
+  final String uid;
   final String gender;
   final int age;
   final double height;
   final double weight;
 
   const Profile({
+    required this.uid,
     required this.gender,
     required this.age,
     required this.height,
     required this.weight,
   });
 
-  Profile copyWith({String? gender, int? age, double? height, double? weight}) {
+  Profile copyWith({
+    String? uid,
+    String? gender,
+    int? age,
+    double? height,
+    double? weight,
+  }) {
     return Profile(
+      uid: uid ?? this.uid,
       gender: gender ?? this.gender,
       age: age ?? this.age,
       height: height ?? this.height,
@@ -20,8 +29,30 @@ class Profile {
     );
   }
 
+  Map<String, dynamic> toJson() {
+    return {
+      'uid': uid,
+      'gender': gender,
+      'age': age,
+      'height': height,
+      'weight': weight,
+      'createdAt': DateTime.now().toIso8601String(),
+      'updatedAt': DateTime.now().toIso8601String(),
+    };
+  }
+
+  factory Profile.fromJson(Map<String, dynamic> json) {
+    return Profile(
+      uid: json['uid'] as String,
+      gender: json['gender'] as String,
+      age: json['age'] as int,
+      height: (json['height'] as num).toDouble(),
+      weight: (json['weight'] as num).toDouble(),
+    );
+  }
+
   static Profile empty() {
-    return Profile(gender: '', age: 0, height: 0, weight: 0);
+    return Profile(uid: "", gender: "", age: 0, height: 0, weight: 0);
   }
 
   bool get isValid {
@@ -30,21 +61,6 @@ class Profile {
 
   bool get isEmpty {
     return gender.isEmpty && age == 0 && height == 0 && weight == 0;
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    return other is Profile &&
-        other.gender == gender &&
-        other.age == age &&
-        other.height == height &&
-        other.weight == weight;
-  }
-
-  @override
-  int get hashCode {
-    return gender.hashCode ^ age.hashCode ^ height.hashCode ^ weight.hashCode;
   }
 
   // business logics
