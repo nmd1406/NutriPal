453248ab3472d7ccb8a4779145dd78fa135bda5d
Nguyễn Minh Đức <148557640+nmd1406@users.noreply.github.com>
2025-07-17 19:49:00 +0700

    placeholders

---
+++ lib/views/screens/more_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MoreScreen extends StatelessWidget {
+  const MoreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("More"));
+    ;
+  }
+}
