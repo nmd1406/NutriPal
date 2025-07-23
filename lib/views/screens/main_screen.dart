b8679a21731a2a856ebdd66e024582cb701e04e5
Nguyễn Minh Đức <148557640+nmd1406@users.noreply.github.com>
2025-07-23 23:57:34 +0700

    refactor

--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:nutripal/viewmodels/auth_viewmodel.dart';
 import 'package:nutripal/viewmodels/navigation_viewmodel.dart';
 import 'package:nutripal/views/screens/dashboard_screen.dart';
 import 'package:nutripal/views/screens/diary_screen.dart';
@@ -12,7 +11,6 @@ class MainScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authState = ref.watch(authViewModelProvider);
     final currentIndex = ref.watch(navigationViewModelProvider);
 
     final screens = const [
@@ -23,45 +21,6 @@ class MainScreen extends ConsumerWidget {
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 70,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 12, top: 18),
-            child: IconButton.filled(
-              onPressed: () {},
-              style: IconButton.styleFrom(
-                backgroundColor: Theme.of(context).primaryColor,
-              ),
-              icon: const Icon(Icons.notifications, color: Colors.white),
-            ),
-          ),
-        ],
-        title: authState.when(
-          data: (user) => Padding(
-            padding: const EdgeInsets.only(top: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Chào mừng,",
-                  style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-                ),
-                Text(
-                  user?.username ?? "User",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.secondary,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 28,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          error: (error, stackTrace) => const Text("Lỗi"),
-          loading: () => const Text("Đang tải..."),
-        ),
-      ),
       body: AnimatedSwitcher(
         duration: const Duration(milliseconds: 300),
         transitionBuilder: (Widget child, Animation<double> animation) {
