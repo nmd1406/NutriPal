aee7a8054ad366c216bfb2ce7ae16b80a0742533
Nguyễn Minh Đức <mduc14062003@gmail.com>
2025-08-20 16:45:14 +0700

    refactor and add json serialize methods

--- lib/models/weight_record.dart
+++ lib/models/weight_record.dart
@@ -1,11 +1,20 @@
+import 'dart:async' show Completer;
 import 'dart:io' show File;
+
+import 'package:flutter/cupertino.dart';
 
 class WeightRecord {
   final double weight;
   final DateTime date;
   final File? image;
+  final String? imageUrl;
 
-  const WeightRecord({required this.weight, required this.date, this.image});
+  const WeightRecord({
+    required this.weight,
+    required this.date,
+    this.image,
+    this.imageUrl,
+  });
 
   static const _noChange = Object();
 
@@ -13,10 +22,12 @@ class WeightRecord {
     double? weight,
     DateTime? date,
     Object? image = _noChange,
+    String? imageUrl,
   }) => WeightRecord(
     weight: weight ?? this.weight,
     date: date ?? this.date,
     image: image == _noChange ? this.image : image as File?,
+    imageUrl: imageUrl ?? this.imageUrl,
   );
 
   String get formattedDate => "${date.day}/${date.month}/${date.year}";
@@ -26,10 +37,39 @@ class WeightRecord {
     }
     return weight.toStringAsFixed(1);
   }
+
+  String formatDateForChart(Period period) {
+    switch (period) {
+      case Period.month:
+      case Period.threeMonths:
+      case Period.sixMonths:
+        return '${date.day}/${date.month}';
+      case Period.year:
+      case Period.all:
+        return '${date.month}/${date.year}';
+    }
+  }
+
+  Map<String, dynamic> toJson() => {
+    "weight": weight,
+    "date": date.toIso8601String(),
+    "imageUrl": imageUrl,
+  };
+
+  factory WeightRecord.fromJson(Map<String, dynamic> json) {
+    return WeightRecord(
+      weight: (json["weight"] as num).toDouble(),
+      date: DateTime.parse(json["date"] as String),
+      imageUrl: json["imageUrl"] as String?,
+    );
+  }
+
+  static WeightRecord empty() {
+    return WeightRecord(weight: 0.0, date: DateTime.now());
+  }
 }
 
 enum Period {
-  week("Tuần"),
   month("1 tháng"),
   threeMonths("3 tháng"),
   sixMonths("6 tháng"),
